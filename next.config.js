5ea5151f9cdce0f6dd3661e2c416d36c4287873d
Rudy Hartt <rudyhartt@Mac.local>
2025-08-24 09:24:33 +0100

    Clean next.config.js: remove deprecated experimental.appDir, add swcMinify

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true
-  },
+  swcMinify: true, // good to add for smaller builds
   images: {
-    domains: ["c1.scryfall.com", "img.scryfall.com"]
-  }
+    domains: ["c1.scryfall.com", "img.scryfall.com"],
+  },
 };
 
 module.exports = nextConfig;
